b9bff1cb61f4aae44e6a999e0044e65604b1ed09
Kamil Grondys <kamil.grondys@codecool.com>
2020-10-04 09:28:51 +0200

    catching up with yesterday
    Adding add_to_inventory and 2 other functions

--- game_inventory.py
+++ game_inventory.py
@@ -6,16 +6,31 @@
 
 def display_inventory(inventory):
     """Display the contents of the inventory in a simple way."""
+    for key, value in inventory.items():
+        print(f"{key}: {value}")
+
     pass
 
 
 def add_to_inventory(inventory, added_items):
     """Add to the inventory dictionary a list of items from added_items."""
+    for item in added_items:
+        if item in inventory.keys():
+            inventory[item] += 1
+        else:
+            inventory[item] = 1
     pass
 
 
 def remove_from_inventory(inventory, removed_items):
     """Remove from the inventory dictionary a list of items from removed_items."""
+    for item in removed_items:
+        if item in inventory.keys() and inventory[item] > 1:
+            inventory[item] -= 1
+        elif item not in inventory:
+            print(f"You can't remove {item} from inventory")
+        else:
+            del inventory[item]
     pass
 
 
